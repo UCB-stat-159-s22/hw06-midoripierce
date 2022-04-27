f455e01682b044ba695d3608ade4be8d8b9200fc
Midori Pierce <midoripierce@berkeley.edu>
2022-04-27 02:59:39 +0000

    update test_readligo

--- ligotools/test/tests_readligo.py
+++ ligotools/test/tests_readligo.py
@@ -8,14 +8,23 @@ def test_loaddata1():
     
     strain_H1, time_H1, chan_dict_H1 = rl.loaddata(fn_H1, 'H1')
     assert type(strain_H1) == np.ndarray
-    assert type(time_H1) == np.ndarray
-    assert type(chan_dict_H1) == dict
     
 def test_loaddata2():
-    fn_L1 = "/home/jovyan/Homework/hw06-midoripierce/data/L-L1_LOSC_4_V2-1126259446-32.hdf5"
+    fn_H1 = "/home/jovyan/Homework/hw06-midoripierce/data/H-H1_LOSC_4_V2-1126259446-32.hdf5"
+    
+    strain_H1, time_H1, chan_dict_H1 = rl.loaddata(fn_H1, 'H1')
+    assert type(time_H1) == np.ndarray
+    assert type(chan_dict_H1) == dict
+
+def test_loaddata3():
+	fn_L1 = "/home/jovyan/Homework/hw06-midoripierce/data/L-L1_LOSC_4_V2-1126259446-32.hdf5"
     
     strain_L1, time_L1, chan_dict_L1 = rl.loaddata(fn_L1, 'L1')
     assert type(strain_L1) == np.ndarray
+
+def test_loaddata4():
+	fn_L1 = "/home/jovyan/Homework/hw06-midoripierce/data/L-L1_LOSC_4_V2-1126259446-32.hdf5"
+    
+    strain_L1, time_L1, chan_dict_L1 = rl.loaddata(fn_L1, 'L1')
     assert type(time_L1) == np.ndarray
     assert type(chan_dict_L1) == dict
-
